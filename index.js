97644000a892462cad42d9618efe51ed5c71e04d
Vazhin Tayeb <vazhintayeb@gmail.com>
2020-06-04 17:34:08 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,3 +3,8 @@ const returnFirstTwoDrivers = function (arrOfDrivers) {
   let newArr = [arrOfDrivers[0], arrOfDrivers[1]]
   return newArr
 }
+
+const returnLastTwoDrivers = function(arrOfDrivers) {
+  let newArr = []
+  
+}
