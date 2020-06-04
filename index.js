d8be9357f01ffa1ad7f3ebf872668a20057bf69a
Vazhin Tayeb <vazhintayeb@gmail.com>
2020-06-04 17:44:03 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -14,6 +14,6 @@ const returnLastTwoDrivers = function(arrOfDrivers) {
 }
 
 let selectingDrivers = [
-  returnFirstTwoDrivers(),
-  returnLastTwoDrivers();
+  returnFirstTwoDrivers,
+  returnLastTwoDrivers;
 ]
